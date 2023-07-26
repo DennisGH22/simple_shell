5cd2ae701c08768bed73633adc8143536b8bdbb1
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-07-26 03:28:48 +0200

    shell

--- shell.h
+++ shell.h
@@ -1,16 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGS 10
-
-void executeCommand(char** args);
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 10
+
+void executeCommand(char** args);
 
 #endif
