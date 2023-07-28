b8e49c71adcde4e0459726f1d29c79683392c548
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-07-28 04:12:39 +0200

    simple_shell

---
+++ get_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * empty_line - checks if a line is empty (contains only spaces).
+ * @line: line
+ *
+ * Return: 0 if the line is not empty, 1 if it is empty.
+*/
+
+int _getline(char *line)
+{
+	if (line == NULL)
+		return (1);
+
+	while (*line)
+	{
+		if (*line != ' ')
+			return (0);
+		line++;
+	}
+
+	return (1);
+}
