5915ccaa3955d79b1dff13c14f210aa065d20f9e
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-07-28 04:24:43 +0200

    simple_shell

--- main.c
+++ main.c
@@ -1,13 +1,53 @@
-#include <stdio.h>
+#include "shell.h"
 
 /**
- * main - Entry point.
+ * main - Simple shell main function.
  *
- * Return: Always 0 (Success).
+ * Return: Exit status.
 */
 
 int main(void)
 {
-	printf("Hello, world!\n");
-	return (0);
+	char *buff = NULL, **args;
+	size_t read_size = 0;
+	ssize_t buff_size = 0;
+	int exit_status = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		buff_size = getline(&buff, &read_size, stdin);
+		if (buff_size == -1 || _strcmp("exit\n", buff) == 0)
+		{
+			free(buff);
+			break;
+		}
+		buff[buff_size - 1] = '\0';
+
+		if (_strcmp("env", buff) == 0)
+		{
+			_env();
+			continue;
+		}
+
+		if (empty_line(buff))
+		{
+			exit_status = 0;
+			continue;
+		}
+
+		args = split_string(buff, " ");
+		args[0] = env_path(args[0]);
+
+		if (args[0] != NULL)
+			exit_status = execute_command(args);
+		else
+			perror("Error");
+
+		free(args);
+	}
+
+	return (exit_status);
 }
